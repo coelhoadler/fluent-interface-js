105a2cb9dff1d316759cd551382dc4c8ee52ea5f
Adler Coelho Santos <adler.coelho.santos@icloud.com>
2021-03-26 14:57:34 -0300

    feat: terminando TDD do fluent interface

--- src/fluentSQL.js
+++ src/fluentSQL.js
@@ -1,6 +1,10 @@
 export default class FluentSQLBuilder {
     
     #database = [];
+    #limit = 0;
+    #select = [];
+    #where = [];
+    #orderBy = '';
 
     constructor({database}) {
         this.#database = database;
@@ -10,9 +14,81 @@ export default class FluentSQLBuilder {
         return new FluentSQLBuilder({ database });
     }
     
+    limit(max) {
+        this.#limit = max;
+
+        return this
+    }
+
+    select(props) {
+        this.#select = props;
+
+        return this
+    }
+
+    where(query) {
+        const [[key, value]] = Object.entries(query);
+        const whereFilter = value instanceof RegExp ? value : new RegExp(value)
+
+        this.#where.push({ key, filter: whereFilter});
+
+        return this
+    }
+
+    orderBy(field) {
+        this.#orderBy = field;
+
+        return this
+    }
+
+    #performWhere(item) {
+        for (const { filter, key } of this.#where) {
+            if (!filter.test(item[key])) return false
+        }
+        return true
+    }    
+
+    #performLimit(results) {
+        return this.#limit && results.length === this.#limit
+    }
+
+    #performSelect(item) {
+        const currentItem = {};
+        const entries = Object.entries(item);
+        
+        for (const [key, value] of entries) {
+            if (this.#select.length && !this.#select.includes(key)) continue;
+
+            currentItem[key] = value;
+        }
+
+        return currentItem;
+    }
+
+    #performOrderBy(results) {
+        if (!this.#orderBy) return results;
+        return results.sort((prev, next) => {
+            return (prev[this.#orderBy]).localeCompare(next[this.#orderBy]);
+        })
+    }
+
     build() {
         const results = [];
-        return results;
+
+        for (const item in this.#database) {
+            const obj = this.#database[item];
+
+            if (!this.#performWhere(obj)) continue
+            
+            const currentItem = this.#performSelect(obj)
+
+            results.push(currentItem);
+
+            if (this.#performLimit(results)) break
+        }
+
+        const finalResult = this.#performOrderBy(results);
+        return finalResult;
     }
 
 }
--- test/fluentSQL.test.js
+++ test/fluentSQL.test.js
@@ -8,15 +8,20 @@ const data = [
         category: 'developer'
     },
     {
+        id: 2,
+        name: 'Ilma',
+        category: 'manager'
+    },    
+    {
         id: 1,
         name: 'Adriane',
         category: 'developer'
     },
     {
-        id: 2,
-        name: 'Ilma',
-        category: 'manager'
-    }        
+        id: 3,
+        name: 'Rafael',
+        category: 'SM'
+    }    
 ];
 
 describe('Test suite for FluentSQL builder', () => {
@@ -30,19 +35,82 @@ describe('Test suite for FluentSQL builder', () => {
 
     test('#build should return empty object instance', () => {
         const result = new FluentSQLBuilder({ database: data }).build();
-        const expected = [];
+        const expected = data;
         
         expect(result).toStrictEqual(expected);
     });
 
-    test.todo('#limit given a collection it should limit results');
+    test('#limit given a collection it should limit results', () => {
+        const result = new FluentSQLBuilder({ database: data }).limit(1).build();
 
-    test.todo('#where given a collection it should filter data');
+        const expected = [data[0]];
+        
+        expect(result).toStrictEqual(expected);
+    });
 
-    test.todo('#select given a collection it should return only specific fields');
+    test('#where given a collection it should filter data', () => {
+        const result = new FluentSQLBuilder({ database: data }).where({
+            category: /^man/
+        }).build();
 
-    test.todo('#orderBy given a collection it should return order results by field');
+        const expected = data.filter(({category}) => category.slice(0, 3) == 'man');
 
-    test.todo('#pipeline');
+        expect(result).toStrictEqual(expected);
+    });
+
+    test('#select given a collection it should return only specific fields', () => {
+        const result = new FluentSQLBuilder({ database: data }).select(['id', 'name']).limit(1).build();
+
+        const expected = [{
+            id: 0,
+            name: 'Adler'
+        }];
+
+        expect(result).toStrictEqual(expected);
+    });
+
+    test('#orderBy given a collection it should return order results by field', () => {
+        const result = new FluentSQLBuilder({ database: data }).orderBy('name').build();
+
+        // lista ordenada
+        const expected = [
+            {
+                id: 0,
+                name: 'Adler',
+                category: 'developer'
+            },
+            {
+                id: 1,
+                name: 'Adriane',
+                category: 'developer'
+            },
+            {
+                id: 2,
+                name: 'Ilma',
+                category: 'manager'
+            },            
+            {
+                id: 3,
+                name: 'Rafael',
+                category: 'SM'
+            }
+        ];
+
+        expect(result).toStrictEqual(expected);
+    });
+
+    test('#pipeline', () => {
+        const result = new FluentSQLBuilder({ database: data })
+        .where({category: 'developer'})
+        .orderBy('name')
+        .select(['name', 'category'])
+        .limit(1)
+        .build();
+
+        const expected = data.filter(({ id }) => id === 0).map(({name, category}) => ({name, category}));
+
+        console.log('expected', expected)
+        expect(result).toStrictEqual(expected);
+    });
     
 });
